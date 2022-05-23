7fa4377d016444739674a1b0273cae6975826f31
Waskertyioup <abel.sierra_l@mail.udp.cl>
2022-05-23 17:20:42 -0400

    comparar usuarios y contarlos sobre 6000

--- consumer/index.js
+++ consumer/index.js
@@ -30,6 +30,11 @@ const topic = "logs"
 
 const consumer = kafka.consumer({ groupId: clientId })
 
+usersBlocked = []
+
+// Array que almacena los intentos de login
+tries = []
+
 // Funcion para leer los mensajes del topic
 const consume = async () => {
 	// first, we wait for the client to connect and subscribe to the given topic
@@ -38,17 +43,54 @@ const consume = async () => {
 	await consumer.run({
 		// this function is called every time the consumer gets a new message
 		eachMessage: ({ message }) => {
+
+
+
+
 			// here, we just log the message to the standard output
-			console.log(`received message: ${message.value}`)
+			user = message.key
+			date = message.value
+
+			console.log(`Usuario: ${user}`)
+			console.log(`Time: ${date}`)
+
+			datalog = [user.toString(), date];
+
+			tries.push(datalog);
+
+			// Revisa los Tries, buscando Banear
+			date_acct = date = Date.now();
+			for (var i = 0; i < tries.length; i++){
+				resta = parseInt(date_acct) - parseInt(tries[i][1]);
+				console.log(`Delay: ${resta}`)
+			}
+			/*
+
+			
+			*/
+
+			/*
+			if (attempts >= 5) {
+				if (!usersBlocked.includes(user.toString())) {
+					usersBlocked.push(user.toString())
+					console.log(user.toString() + " bloqueado")
+				}
+			}
+			*/
 		},
 	})
 }
 
-
 // Lee los mensajes en topic
 app.get("/cons", async (req, res) => {
   consume();
+  res.send("consumiendo") 
 });
 
+app.get('/blocked', (req, res) => {
+	  res.send(JSON.stringify({ "users-blocked": usersBlocked }));
+});
 
-
+app.get('/tries', (req, res) => {
+	res.send(JSON.stringify({ "tries": tries }));
+});
--- producer/index.js
+++ producer/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { Kafka } = require('kafkajs')
+// const request = require("request");
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -27,39 +28,97 @@ app.listen(port, () => {
 const clientId = "producer"
 // we can define the list of brokers in the cluster
 const brokers = ["0.0.0.0:9092"]
-// Declaracion del topi
+// Declaracion del topic
 const topic = "logs"
 
 
-// Funcion para escribir en el topic
-const produce = async () => {
-	await producer.connect()
-	let i = 0
+// // Funcion para escribir en el topic
+// const produce = async () => {
+// 	await producer.connect()
+// 	let i = 0
 
-	// after the produce has connected, we start an interval timer
-	setInterval(async () => {
-		try {
-			// send a message to the configured topic with
-			// the key and value formed from the current value of `i`
-			await producer.send({
-				topic,
-				messages: [
-					{
-						key: String(i),
-						value: "this is message " + i,
-					},
-				],
-			})
+// 	// after the produce has connected, we start an interval timer
+// 	setInterval(async () => {
+// 		try {
+// 			// send a message to the configured topic with
+// 			// the key and value formed from the current value of `i`
+// 			await producer.send({
+// 				topic,
+// 				messages: [
+// 					{
+// 						key: String(i),
+// 						value: "this is message " + i,
+// 					},
+// 				],
+// 			})
 
-			// if the message is written successfully, log it and increment `i`
-			console.log("writes: ", i)
-			i++
-		} catch (err) {
-			console.error("could not write message " + err)
-		}
-	}, 1000)
+// 			// if the message is written successfully, log it and increment `i`
+// 			console.log("writes: ", i)
+// 			i++
+// 		} catch (err) {
+// 			console.error("could not write message " + err)
+// 		}
+// 	}, 1000)
+// }
+//var i = 0
+const produce = async (req) => {
+	  await producer.connect()
+	//   let i = 0
+	  const { user, pass } = req.body
+	  try {
+		// Seconds from epoch
+		date = Date.now();
+		//i++
+	    await producer.send({
+	      topic,
+	      messages: [
+	        { key: user,
+			  value: date.toString()
+			},
+	      ],
+	    })
+		console.log("writes: ", user, date)
+	  } catch (err) {
+		  console.error("could not write message " + err)
+	  }
 }
 
-app.get("/login", async (req, res) => {
+app.get("/prod", async (req, res) => {
   produce();
 });
+
+
+app.post('/login', async (req, res) => {
+	const { user, pass } = req.body;
+	// res.send("POST Request Called")
+	produce(req);
+	res.send('user: ' + user + ' pass: ' + pass);
+})
+
+// app.post("/login", async (req, res) => {
+// 	  console.log("Test");
+// 	  const { user, pass } = req.body;
+// 	  const url = "http://localhost:3000/login";
+// 	  const options = {
+// 		  method: "POST",
+// 		  url: url,
+// 		  headers: {
+// 			  "Content-Type": "application/json",
+// 		  },
+// 		  body: {
+// 			  user: user,
+// 			  pass: pass,
+// 		  },
+// 		};
+// });
+
+// request.post(
+// 	  "http://localhost:3000/login",
+// 	  { json: { user: "admin", pass: "456" } },
+// 	  (error, response, body) => {
+// 		if (error) {
+// 		  return console.log(error);
+// 		}
+// 		console.log(body);
+// 	  }
+// );
